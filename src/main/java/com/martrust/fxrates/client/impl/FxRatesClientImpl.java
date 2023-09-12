3aef84cd4dfa9a29d6bf4f43f7b577d85b575d54
dvlbamasa <dvlbamasa@gmail.com>
2023-09-12 15:37:11 +0800

    remove redundant client classes

--- src/main/java/com/martrust/fxrates/client/impl/FxRatesClientImpl.java
+++ src/main/java/com/martrust/fxrates/client/impl/FxRatesClientImpl.java
@@ -1,30 +1,28 @@
 package com.martrust.fxrates.client.impl;
 
-import com.martrust.fxrates.client.ExchangeRateClient;
+import com.martrust.fxrates.client.ExchangeRateFeign;
 import com.martrust.fxrates.model.RateResponse;
 import com.martrust.fxrates.model.SymbolsResponse;
 import com.martrust.fxrates.client.FxRateClient;
 import com.martrust.fxrates.util.FxRatesUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class FxRatesClientImpl implements FxRateClient {
 
     @Value("${exchange.rate.io.access.key}")
     private String ACCESS_KEY_VALUE;
 
-    private final ExchangeRateClient exchangeRateClient;
-
-    public FxRatesClientImpl(ExchangeRateClient exchangeRateClient) {
-        this.exchangeRateClient = exchangeRateClient;
-    }
+    private final ExchangeRateFeign exchangeRateFeign;
 
     @Override
     public SymbolsResponse getSymbols() {
         SymbolsResponse symbolsResponse = new SymbolsResponse();
         try {
-            symbolsResponse = exchangeRateClient.getSymbols(ACCESS_KEY_VALUE);
+            symbolsResponse = exchangeRateFeign.getSymbols(ACCESS_KEY_VALUE);
         } catch (Exception exception) {
             FxRatesUtil.responseHandler(symbolsResponse);
             throw exception;
@@ -36,7 +34,7 @@ public class FxRatesClientImpl implements FxRateClient {
     public RateResponse fetchRate(String base, String to) {
         RateResponse rateResponse = new RateResponse();
         try {
-            rateResponse = exchangeRateClient.getRates(ACCESS_KEY_VALUE, base, to);
+            rateResponse = exchangeRateFeign.getRate(ACCESS_KEY_VALUE, base, to);
         } catch (Exception exception) {
             FxRatesUtil.responseHandler(rateResponse);
             throw exception;
